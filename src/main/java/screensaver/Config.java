5e7e3eaeaedf18d651cdc4501ba5fb9371dc5107
andrulian <andvladul@gmail.com>
2017-12-03 23:23:13 +0300

    PeriodicalScopeConfigurer was updated

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -16,7 +16,7 @@ public class Config {
 
     @Bean
 //    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
