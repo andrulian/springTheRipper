3f70e1e208653573d6d2f52dd77fcfb8f0962f36
andrulian <andvladul@gmail.com>
2017-12-03 20:20:39 +0300

    ProfilingController & ProfilingControllerMBean was added

--- src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoters/ProfilingHandlerBeanPostProcessor.java
@@ -3,6 +3,8 @@ package quoters;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,9 +23,14 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor{
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         Class beanClass = map.get(beanName);
         if (beanClass != null) {
-            return Proxy.newProxyInstance();
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(),
+                    new InvocationHandler() {
+                        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                            return null;
+                        }
+                    });
         }
-        
+
         return bean;
     }
 }
