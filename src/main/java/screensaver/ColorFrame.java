60d145f0c4c53f77444f380db60c27106c6ea258
andrulian <andvladul@gmail.com>
2017-12-03 22:56:55 +0300

    Color issues was solved

--- src/main/java/screensaver/ColorFrame.java
+++ src/main/java/screensaver/ColorFrame.java
@@ -1,15 +1,17 @@
 package screensaver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 @org.springframework.stereotype.Component
-public class ColorFrame extends JFrame {
-    @Autowired
-    private Color color;
+//@Scope("prototype")
+public abstract class ColorFrame extends JFrame {
+//    @Autowired
+//    private Color color;
 
     public ColorFrame() {
         setSize(200, 200);
@@ -20,9 +22,12 @@ public class ColorFrame extends JFrame {
     public void showOnRandomPlace() {
         Random random = new Random();
         setLocation(random.nextInt(1200), random.nextInt(700));
-        getContentPane().setBackground(color);
+//        getContentPane().setBackground(color);
+        getContentPane().setBackground(getColor());
         repaint();
     }
+
+    protected abstract Color getColor();
 
 
 }
--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import java.awt.*;
 import java.util.Random;
@@ -13,9 +15,21 @@ import java.util.Random;
 public class Config {
 
     @Bean
+//    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @Scope("prototype")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame frame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
     }
 
     public static void main(String[] args) throws InterruptedException {
