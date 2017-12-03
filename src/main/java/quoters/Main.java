0872d51a647291b0afeee4a0074a81ed1017bddf
andrulian <andvladul@gmail.com>
2017-12-03 22:12:43 +0300

    PropertyFileApplicationContext was added

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(Quoter.class).sayQuote();
 
 //        while (true) {
 //            Thread.sleep(3000);
---
+++ src/main/java/quoters/PropertyFileApplicationContext.java
@@ -0,0 +1,11 @@
+package quoters;
+
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class PropertyFileApplicationContext extends GenericApplicationContext{
+    public PropertyFileApplicationContext(String fileName) {
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(this);
+        reader.loadBeanDefinitions(fileName)
+    }
+}
