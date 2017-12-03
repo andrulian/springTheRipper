0819cec14ccf935e98670959527af7c5f35500c2
andrulian <andvladul@gmail.com>
2017-12-03 21:31:09 +0300

    PostProxyInvokerContextListener was added

--- src/main/java/quoters/PostProxyInvokerContextListener.java
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -1,4 +1,39 @@
 package quoters;
 
-public class PostProxyInvokerContextListener {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import java.lang.reflect.Method;
+
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent>{
+    @Autowired
+    private ConfigurableListableBeanFactory factory;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        ApplicationContext context = contextRefreshedEvent.getApplicationContext();
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names) {
+            BeanDefinition beanDefinition = factory.getBeanDefinition(name);
+            String className = beanDefinition.getBeanClassName();
+            try {
+                Class<?> aClass = Class.forName(className);
+                Method[] methods = aClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(PostProxy.class)) {
+                        Object bean = context.getBean(name);
+                        Method currentMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
+                        currentMethod.invoke(bean);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
